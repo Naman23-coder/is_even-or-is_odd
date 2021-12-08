9a4b2de0ecf8c8a9803f9e320eccc8687f150672
Naman Sharma <namansharma232009@gmail.com>
2021-12-08 17:33:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
-def is_even(number):
+def is_even(number:int)->bool:
     if number%2==0:
         return True
+    else: 
+        return False
         
-def is_odd(number):
-     return True if not is_even(number)
+def is_odd(number:int)->bool:
+     return not is_even(number)
